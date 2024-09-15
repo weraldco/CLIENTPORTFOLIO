d55c9cf321454fcbe9037c40b97ae26c98e23112
Werald Opolento <werald.opolento@gmail.com>
2024-09-15 13:55:13 +0800

    update:YML file

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 },
   base: "/CLIENTPORTFOLIO/",
 });
